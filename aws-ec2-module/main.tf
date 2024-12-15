dd4c3ab453542155fcb897a10644610fa3794cff
svcwas <>
2024-12-15 13:53:11 +0530

    updated

--- aws-ec2-module/main.tf
+++ aws-ec2-module/main.tf
@@ -2,6 +2,11 @@ resource "aws_instance" "m_instance" {
   ami           = var.ec2_ami
   instance_type = var.ec2_instances_typ
   key_name      = var.ec2_key_name
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = 30
+    volume_type = "gp2"
+  }
   tags = merge(
     {
       Name = "${var.ec2_instance_name}-${var.common_tags.environment}"
